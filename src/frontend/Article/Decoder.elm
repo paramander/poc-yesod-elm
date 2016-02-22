6d26c11cb5ef45006587592ba87b9f35ba31bd1a
Tolga Paksoy <tolga@hoppinger.com>
2016-02-22 23:29:25 +0100

    Fix silent crash because Yesod returns IDs in string format

--- src/frontend/Article/Decoder.elm
+++ src/frontend/Article/Decoder.elm
@@ -3,10 +3,15 @@ module Article.Decoder where
 import Article.Model as Article exposing (Model)
 
 import Json.Decode as JD exposing ((:=))
+import String
 
 decode : JD.Decoder Article.Model
 decode =
-  JD.object3 Article.Model
-    ("id" := JD.int)
-    ("title" := JD.string)
-    ("content" := JD.string)
+  let
+    number : JD.Decoder Int
+    number = JD.oneOf [ JD.int, JD.customDecoder JD.string String.toInt]
+  in
+    JD.object3 Article.Model
+        ("id" := number)
+        ("title" := JD.string)
+        ("content" := JD.string)
